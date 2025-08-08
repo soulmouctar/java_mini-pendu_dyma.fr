ca56ecb12f0a20467e7ae958108df3fb3354e2da
Souleymane Mouctar Diallo <soulmouctar96@gmail.com>
2025-08-08 22:55:44 +0000

    amelioration du jeu du pendu

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,29 @@ import com.dyma.game.GuessGame;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Main class to run the Guess Game.
+ * The game allows players to guess letters of a secret word within a limited number of attempts.
+ */
 public class Main {
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
         final var random = new Random();
         final var words = "abuser crotte continental alpha yaya mamadou école ingenieur".split(" ");
-        final var wordToGuess = words[random.nextInt(words.length)];
-        final var game = new GuessGame(wordToGuess, 10);
+        var wordToGuess = words[random.nextInt(words.length)];
+        var game = new GuessGame(wordToGuess, 10);
 
         System.out.println("DEBUT DU JEU DE PENDU");
 
         while (true) {
             System.out.println(game);
-            final var letter = scanLetter();
-
+            final var letter = scanLetter("Entrez une lettre : ");
             game.guessLetter(letter);
 
             if (game.isLost()) {
                 System.out.println("Vous avez perdu !");
                 System.out.println(game);
-                //System.out.println("Le mot était : " + wordToGuess);
-                break;
+                System.out.println("Le mot était : " + wordToGuess);
             }
 
             if (game.isWon()) {
@@ -31,15 +33,26 @@ public class Main {
                 System.out.println("Félicitations, vous avez gagné !");
                 break;
             }
+
+            if(game.isLost() || game.isWon()) {
+                var replay = scanLetter("Voulez vous rejouer ? (o/n) : ");
+                if(replay == 'o' || replay == 'O') {
+                    wordToGuess = words[random.nextInt(words.length)];
+                    game = new GuessGame(wordToGuess, 10);
+                } else {
+                    System.out.println("Merci d'avoir joué !");
+                    break;
+                }
+            }
         }
 
     }
 
-    private static char scanLetter() {
+    private static char scanLetter(String question) {
         final var scanner = new Scanner(System.in);
         Character letter = null;
         do{
-            System.out.println("Entrez une lettre : ");
+            System.out.println(question);
             var input = scanner.nextLine();
             if (input.length() == 1) {
                 letter = input.charAt(0);
--- src/com/dyma/game/GuessGame.java
+++ src/com/dyma/game/GuessGame.java
@@ -1,14 +1,33 @@
 package com.dyma.game;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * TO DO
+ */
 public class GuessGame {
-    private final List<Character> secretWord = new ArrayList<>();
-    private int lifePoints;
-    private final List<Character> guessWord = new ArrayList<>();
 
+    /**
+     * The secret word to guess, represented as a list of characters.
+     */
+    private final List<Character> secretWord = new ArrayList<>();
+    /**
+     * The number of life points remaining for the player.
+     */
+    private int lifePoints;
+    /**
+     * The current state of the guessed word, represented as a list of characters.
+     */
+    private final List<Character> guessWord = new ArrayList<>();
+    /**
+     * The set of letters that have been guessed by the player.
+     */
+    private final Set<Character> guessLetter = new HashSet<>();
+
+    /**
+     * @param wordToGuess
+     * @param lifePoints
+     */
     public GuessGame(String wordToGuess, int lifePoints) {
         for (char c : wordToGuess.toCharArray()) {
             this.secretWord.add(c);
@@ -24,10 +43,12 @@ public class GuessGame {
         return "GuessGame{" +
                 "lifePoints=" + lifePoints +
                 ", guessWord=" + guessWord +
+                ", guessLetter=" + guessLetter +
                 '}';
     }
 
     public void guessLetter(char letter) {
+        guessLetter.add(letter);
         if(secretWord.contains(letter) && !guessWord.contains(letter)) {
             var index = 0;
             for (char c : secretWord) {
